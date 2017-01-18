197217ac57a9e7c363acffdf5f68e74a2e003537
Ashok Siyani <ashok.siyani@gmail.com>
2017-01-18 23:13:52 +0000

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicPath = path.join(__dirname, '../public');
+
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=> {
+    console.log('Listioning to PORT:',port);
+})
+
+
